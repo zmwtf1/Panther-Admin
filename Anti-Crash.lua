337f4fc0458a3a8633aa70fb8ed47e0670b2033d
ZMWTF <zmwtf1@hotmail.com>
2025-10-22 18:45:05 -0400

    Create Anti-Crash.lua

---
+++ Anti-Crash.lua
@@ -0,0 +1,23 @@
+-- ANTI-CRASH TEST
+
+-- Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- REMOTE EVENTS
+local ShootEvent = ReplicatedStorage.GunRemotes.ShootEvent -- RemoteEvent 
+local ReplicateEvent = ReplicatedStorage.GunRemotes.ReplicateEvent -- RemoteEvent 
+
+-- ACTUAL CODE
+for _, Connection in getconnections(ReplicateEvent.OnClientEvent) do
+    Connection:Disable()
+end
+
+local Old; Old = hookmetamethod(game, "__namecall", function(self, ...)
+    local Method = getnamecallmethod()
+    if self == ShootEvent and Method == "FireServer" then
+        return
+    end
+    return Old(self, ...)
+end)
+
+print("ANTI-CRASH has been loaded.")
